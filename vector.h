c3042f55073d1c0c949d4bdaad561ec1e483e714
Santi0699 <santisabsol06@gmail.com>
2025-09-21 22:21:33 -0300

    Se agrega class vector

---
+++ vector.h
@@ -0,0 +1,34 @@
+#ifndef VECTOR_H
+#define VECTOR_H
+
+#include <iostream>
+using namespace std;
+
+class vector
+{
+    private:
+    int* a;
+    int size;
+    int maxsize;
+    
+    public:
+
+    vector(int size=10);
+    ~vector();
+    int vectorget(int index);
+    void vectorset(int index, int value);
+    void vectorinsert(int index, int value);
+    int vectorisfull();
+    void vectorresize();
+    void vectorremove();
+    int vectorfind();
+    void vectoraddback();
+    void vectoraddfront();
+    int vectorgetsize();
+    int vectorgetmaxsize();
+    vector operator+()const;
+    vector operator-()const;
+
+};
+
+#endif
